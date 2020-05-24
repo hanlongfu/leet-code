977268fb76d5fbc2f6e13bbe4388d2725523504f
hanlongfu <hfu@salemstate.edu>
2020-05-24 00:39:28 -0700

    easy #3

--- 414.js
+++ 414.js
@@ -1,1 +1,8 @@
-// Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+// Given a non-empty array of integers, return the third maximum number in this array. If it does not exist, return the maximum number. The time complexity must be in O(n).
+
+var thirdMax = function (nums) {
+	var unique = [...new Set(nums)];
+	return unique.length >= 3
+		? unique.sort((a, b) => b - a)[2]
+		: Math.max(...unique);
+};
